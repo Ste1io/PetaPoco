6fb715f70151b2f07b0ba2cde666590d4ef7f6ab
Stelio Kontos <37424493+Ste1io@users.noreply.github.com>
2023-11-05 19:21:01 -0600

    Refactor PocoDataHelper{T} as specialization of non-generic base class making it more intuitive to use for runtime poco objects. Typed pocos use the specialized generic class as before.

--- PetaPoco/Core/PocoDataHelper.cs
+++ PetaPoco/Core/PocoDataHelper.cs
@@ -8,7 +8,7 @@ namespace PetaPoco
 {
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
-    public class PocoDataHelper<T>
+    public class PocoDataHelper
     {
         private readonly IDatabase _db;
         private readonly Lazy<PocoData> _pd;
@@ -17,35 +17,44 @@ namespace PetaPoco
 
         public PocoData PocoData => _pd.Value;
 
-        public PocoDataHelper(IDatabase database, IMapper defaultMapper = null)
+        public PocoDataHelper(IDatabase database, Type pocoType, IMapper defaultMapper = null)
         {
             _db = database;
-            _pd = new Lazy<PocoData>(() => PocoData.ForType(typeof(T), defaultMapper ?? _db.DefaultMapper));
+            _pd = new Lazy<PocoData>(() => PocoData.ForType(pocoType, defaultMapper ?? _db.DefaultMapper));
         }
 
-        public PocoDataHelper(IDatabase database, object obj, string primaryKeyName, IMapper defaultMapper = null)
+        public PocoDataHelper(IDatabase database, object pocoObject, string primaryKeyName, IMapper defaultMapper = null)
         {
             _db = database;
-            _pd = new Lazy<PocoData>(() => PocoData.ForObject(obj, primaryKeyName, defaultMapper ?? _db.DefaultMapper));
+            _pd = new Lazy<PocoData>(() => PocoData.ForObject(pocoObject, primaryKeyName, defaultMapper ?? _db.DefaultMapper));
         }
-
-        public string EscapedTableName() => _db.Provider.EscapeTableName(PocoData.TableInfo.TableName);
 
         public string EscapeTableName(string tableName) => _db.Provider.EscapeTableName(tableName);
 
         public string EscapedPrimaryKeyName() => _db.Provider.EscapeSqlIdentifier(PocoData.TableInfo.PrimaryKey);
+
+        public string EscapeColumnName(string propertyName)
+        {
+            var colName = _propToColumnMap.GetOrAdd(propertyName,
+                _ => PocoData.Columns.Values.First(c => c.PropertyInfo.Name.Equals(propertyName)).ColumnName);
+            return _columnNameCache.GetOrAdd(colName, _db.Provider.EscapeSqlIdentifier);
+        }
+    }
+
+    public class PocoDataHelper<T> : PocoDataHelper
+    {
+        public PocoDataHelper(IDatabase database, IMapper defaultMapper = null)
+            : base(database, typeof(T), defaultMapper) { }
+
+        public string EscapedTableName() => EscapeTableName(PocoData.TableInfo.TableName);
 
         public string EscapeColumnName(Expression<Func<T, object>> propertyExpression)
         {
             var memberExpr = propertyExpression.Body as MemberExpression
                 ?? ((UnaryExpression)propertyExpression.Body).Operand as MemberExpression;
             var propName = memberExpr.Member.Name;
-            var colName = _propToColumnMap.GetOrAdd(propName,
-                _ => PocoData.Columns.Values.First(c => c.PropertyInfo.Name.Equals(propName)).ColumnName);
-            return _columnNameCache.GetOrAdd(colName, _db.Provider.EscapeSqlIdentifier);
+            return EscapeColumnName(propName);
         }
-
-        public string EscapeColumnName(string columnName) => _db.Provider.EscapeSqlIdentifier(columnName);
     }
 
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
